9c1c60e62d7006e42b97dd0131871499c86b7d09
Michael Oliva <mikejoliva@gmail.com>
2020-03-05 16:03:06 +0000

    Improve coolness
    <*((><

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 $(document).ready(() => {
-  doThing();
+  generateText();
+  moveBackground();
 });
 
-async function doThing() {
+const fontList = [
+  'Georgia, serif',
+  'Palatino Linotype\', \'Book Antiqua\', Palatino, serif',
+  '\'Times New Roman\', Times, serif',
+  'Arial, Helvetica, sans-serif',
+  '\'Arial Black\', Gadget, sans-serif',
+  '\'Comic Sans MS\', cursive, sans-serif',
+  'Impact, Charcoal, sans-serif',
+  '\'Lucida Sans Unicode\', \'Lucida Grande\', sans-serif',
+  'Tahoma, Geneva, sans-serif',
+  '\'Trebuchet MS\', Helvetica, sans-serif',
+  'Verdana, Geneva, sans-serif',
+  '\'Courier New\', Courier, monospace',
+  '\'Lucida Console\', Monaco, monospace'
+]
+
+async function generateText() {
   const myDiv = $('#cool-div');
-  const myBody = $('#body');
   while (true) {
-    myDiv.append(`<span style="color: ${getRandomColor()}">Please hire me <span>`);
-    $(myBody).css('background-color', getRandomColor());
+    myDiv.append(`<span style="color: ${getRandomColor()}; font-family: ${getRandomFont()}">PLEASE HIRE ME <span>`);
     await sleep(200);
   }
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+async function moveBackground() {
+  const myBody = $('#body');
+  let counter = 0;
+  while (true) {
+    if (counter >= 360) counter = 0;
+    $(myBody).css('background-color', getColour(counter++));
+    await sleep(10);
   }
-  return color;
+}
+
+function getColour(num) {
+  return `hsl(${num}, 100%, 50%)`;
+}
+
+function getRandomColor() {
+  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+}
+
+function getRandomFont() {
+  return fontList[Math.floor(Math.random() * fontList.length)];
 }
 
 function sleep(ms) {
